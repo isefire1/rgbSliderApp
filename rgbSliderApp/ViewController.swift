43c831170ca1e40ebea7feff4d6f4ca8edd7299a
isefire1 <isefire@bk.ru>
2020-10-18 21:19:20 +0300

    final

--- rgbSliderApp/ViewController.swift
+++ rgbSliderApp/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var rSlider: UISlider!
+    @IBOutlet var gSlider: UISlider!
+    @IBOutlet var bSlider: UISlider!
+    
+    @IBOutlet var rValueSlider: UILabel!
+    @IBOutlet var gValueSlider: UILabel!
+    @IBOutlet var bValueSlider: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.layer.cornerRadius = 10
+        
+        rSlider.minimumTrackTintColor = .red
+        gSlider.minimumTrackTintColor = .green
+        
+        rValueSlider.text = showValue(from: rSlider)
+        gValueSlider.text = showValue(from: gSlider)
+        bValueSlider.text = showValue(from: bSlider)
+        
+        collorChanged()
+        
     }
-
-
+    
+    @IBAction func rgbSliderAction() {
+        rValueSlider.text = showValue(from: rSlider)
+        gValueSlider.text = showValue(from: gSlider)
+        bValueSlider.text = showValue(from: bSlider)
+        
+        collorChanged()
+    }
+    
+    private func collorChanged() {
+        mainView.backgroundColor = UIColor(red: CGFloat(rSlider.value),
+                                           green: CGFloat(gSlider.value),
+                                           blue: CGFloat(bSlider.value),
+                                           alpha: 1)
+    }
+    
+    private func showValue(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
+
 
