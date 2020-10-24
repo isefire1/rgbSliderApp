a0d543d52cf62198caf7806659c7792c5f8dd387
isefire1 <isefire@bk.ru>
2020-10-24 06:01:18 +0300

    final

--- rgbSliderApp/ViewController.swift
+++ rgbSliderApp/ViewController.swift
@@ -27,20 +27,22 @@ class ViewController: UIViewController {
         rSlider.minimumTrackTintColor = .red
         gSlider.minimumTrackTintColor = .green
         
-        rValueSlider.text = showValue(from: rSlider)
-        gValueSlider.text = showValue(from: gSlider)
-        bValueSlider.text = showValue(from: bSlider)
+        correctValue(for: rValueSlider, gValueSlider, bValueSlider)
         
         collorChanged()
         
     }
     
-    @IBAction func rgbSliderAction() {
-        rValueSlider.text = showValue(from: rSlider)
-        gValueSlider.text = showValue(from: gSlider)
-        bValueSlider.text = showValue(from: bSlider)
-        
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
         collorChanged()
+        
+        switch sender.tag {
+        case 0: rValueSlider.text = showValue(from: sender)
+        case 1: gValueSlider.text = showValue(from: sender)
+        case 2: bValueSlider.text = showValue(from: sender)
+        default: break
+        }
+
     }
     
     private func collorChanged() {
@@ -49,10 +51,22 @@ class ViewController: UIViewController {
                                            blue: CGFloat(bSlider.value),
                                            alpha: 1)
     }
+
+    private func correctValue(for labels: UILabel...) {
+            labels.forEach { label in
+                switch label.tag {
+                case 0: rValueSlider.text = showValue(from: rSlider)
+                case 1: gValueSlider.text = showValue(from: gSlider)
+                case 2: bValueSlider.text = showValue(from: bSlider)
+                default: break
+                }
+            }
+        }
     
     private func showValue(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
 }
+
 
 
